b052ea6041ae5b33ac179613d66f46ebabbe2b7b
hullale <hullale@oregonstate.edu>
2019-12-08 19:52:44 -0800

    Update .gitignore to include node modules
    Make email optional, added default keys.js

---
+++ keys.js
@@ -0,0 +1,16 @@
+/*
+ *  Uncomment the following parameters and fill in with your
+ *  information to use reddit miner.
+ *  Email, password, and recieveEmail are optional if you with to recieve an email on error. 
+ *  https://docs.mongodb.com/manual/reference/connection-string/
+ */
+
+var atlasURL = {
+    /*url: "your mongoDB Atlas URL with credentials",*/
+    /*email: "OPTIONAL your sender email address",*/
+    /*password: "OPTIONAL your sender email password",*/
+    /*recieveEmail: "OPTIONAL your reciever email address"*/
+}
+
+module.exports = atlasURL;
+
